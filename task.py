6670afaa1898e69a660c3c1b45bd374492eda2e9
DaniyalMahzer <85176106+DaniyalMahzer@users.noreply.github.com>
2023-05-26 07:54:24 +0500

    added logs

--- task.py
+++ task.py
@@ -8,12 +8,14 @@ from variables import search, sections, months
 
 env = os.environ.get('stage')
 if env == 'cloud':
+    print('Processing with workitems.')
     wi = WorkItems()
     wi.get_input_work_item()
     payload = wi.get_work_item_payload()
     sections = payload.get('sections')
     months = payload.get('months', 0)
     search_phrase = payload.get('search', '')
-
+else:
+    print('processing with local variables.')
 scraper = NewsScraper(search, sections, months)
 scraper.start()
